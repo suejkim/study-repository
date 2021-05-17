d7452c09ad2067a63c05de412e2698a9343a4e59
sjkim <abbccc0225@gmail.com>
2021-05-17 23:18:53 +0900

    add log4j.properties, update build.gradle

--- spring-mvc-example/app/src/main/java/com/sjkim/home/HomeController.java
+++ spring-mvc-example/app/src/main/java/com/sjkim/home/HomeController.java
@@ -1,11 +1,14 @@
 package com.sjkim.home;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 
+
+@Slf4j
 @Controller
 public class HomeController {
 
@@ -14,6 +17,10 @@ public class HomeController {
         ModelAndView modelAndView = new ModelAndView("home");
         modelAndView.addObject("title", "HOME");
         modelAndView.addObject("today", LocalDate.now());
+        log.trace(">>TRACE title: {}, today: {}", modelAndView.getModel().get("title"), modelAndView.getModel().get("today"));
+        log.info(">>INFO title: {}, today: {}", modelAndView.getModel().get("title"), modelAndView.getModel().get("today"));
+        log.debug(">>DEBUG title: {}, today: {}", modelAndView.getModel().get("title"), modelAndView.getModel().get("today"));
+        log.error(">>ERROR title: {}, today: {}", modelAndView.getModel().get("title"), modelAndView.getModel().get("today"));
         return modelAndView;
     }
 }
