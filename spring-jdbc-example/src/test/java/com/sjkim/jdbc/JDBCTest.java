2281de931ac9ce37636492a9e4a5c2f1ce640475
sjkim <abbccc0225@gmail.com>
2021-06-17 23:51:50 +0900

    add JDBCTest test code

---
+++ spring-jdbc-example/src/test/java/com/sjkim/jdbc/JDBCTest.java
@@ -0,0 +1,27 @@
+package com.sjkim.jdbc;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class JDBCTest {
+
+    @BeforeAll
+    static void setUp() throws Exception {
+        Class.forName("org.mariadb.jdbc.Driver").getDeclaredConstructor().newInstance();
+    }
+
+    @Test
+    void testConnection() throws Exception {
+        String url = "jdbc:mariadb://localhost:3306/school";
+        String user = "sjkim";
+        String password = "password";
+        Connection conn = DriverManager.getConnection(url, user, password);
+        assertThat(conn, notNullValue());
+    }
+}
