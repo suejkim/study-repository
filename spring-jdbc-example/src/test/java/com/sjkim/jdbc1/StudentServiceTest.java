d8f10c2da8d5c4fc0edc5a5a4e22fd41c15f65ee
sjkim <abbccc0225@gmail.com>
2021-06-23 23:17:19 +0900

    update StudentServiceTest 정리

--- spring-jdbc-example/src/test/java/com/sjkim/jdbc1/StudentServiceTest.java
+++ spring-jdbc-example/src/test/java/com/sjkim/jdbc1/StudentServiceTest.java
@@ -1,51 +1,43 @@
 package com.sjkim.jdbc1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-
 import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class StudentServiceTest {
+
     private final StudentService studentService = new StudentService();
 
     @Test
-    void add() throws Exception {
-        Student student = new Student("김똑순", 10, LocalDate.of(2020, 1, 1), "010-1111-2222");
+    void addStudentTest() throws Exception {
+        Student student = addStudent();
         boolean result = studentService.add(student);
         assertThat(result, is(Boolean.TRUE));
     }
 
     @Test
-    void update() throws Exception {
-        Student student = new Student();
-        student.setId(1);
-        student.setBirth(LocalDate.of(2020, 1,2));
+    void updateStudentTest() throws Exception {
+        LocalDate birth = LocalDate.now().plusDays(1L);
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        Student student = students.get(0);
+        student.setBirth(birth);
         boolean result = studentService.update(student);
         assertEquals(Boolean.TRUE, result);
-    }
 
-    @Test
-    void delete() throws Exception {
-        boolean result = studentService.delete(1L); // 문제
-        assertEquals(Boolean.TRUE, result);
-    }
-
-    @Test
-    void getAll() throws Exception {
-        List<Student> students = studentService.getAll();
-        assertThat(students.size(), equalTo(2)); // 문제
-    }
-
-    @Test
-    void getStudentTest() throws Exception {
-        Student student = studentService.get(2L);
-//        assertThat(student, nullValue());
-        assertThat(student.getId(), is(2L));
+        Student updatedStudent = studentService.get(student.getId());
+        assertThat(student.getId(), equalTo(updatedStudent.getId()));
     }
 
     @Test
@@ -53,5 +45,45 @@ public class StudentServiceTest {
         int count = studentService.countAll();
         List<Student> students = studentService.getAll();
         assertEquals(students.size(), count);
+    }
+
+    @Test
+    void getAllTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        int count = studentService.countAll();
+        assertThat(students.size(), equalTo(count));
+    }
+
+    @Test
+    void deleteStudentTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        long id = students.get(0).getId();
+        boolean result = studentService.delete(id);
+        assertEquals(Boolean.TRUE, result);
+        Student deletedStudent = studentService.get(id);
+        assertNull(deletedStudent);
+        assertThat(deletedStudent, nullValue());
+    }
+
+    @Test
+    void getStudentTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        long id = students.get(0).getId();
+        Student getStudent = studentService.get(id);
+        Assertions.assertEquals(id, getStudent.getId());
+    }
+
+    private Student addStudent() {
+        return new Student("김똑순", 10, LocalDate.now(), "010-1111-2222");
     }
 }
