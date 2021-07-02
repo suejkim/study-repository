0eea303d13df744a433bd53267f10945170a72cf
sjkim <abbccc0225@gmail.com>
2021-07-02 23:45:18 +0900

    update jdbc2 StudentService,Test

--- spring-jdbc-example/src/main/java/com/sjkim/jdbc2/StudentService.java
+++ spring-jdbc-example/src/main/java/com/sjkim/jdbc2/StudentService.java
@@ -3,21 +3,16 @@ package com.sjkim.jdbc2;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-//@RequiredArgsConstructor
 public class StudentService {
 
-    private Connection conn;
-
-    public StudentService() {
-        conn = DBConn.getConnection();
-    }
-
     public boolean add(Student student) {
+        Connection conn = DBConn.getConnection();
         PreparedStatement psm = null;
         String sql = "insert into student(name, age, birth, phone) values (?, ?, ?, ?)";
-
         try {
             psm = conn.prepareStatement(sql);
             psm.setString(1, student.getName());
@@ -36,24 +31,166 @@ public class StudentService {
                 }
             }
         }
-
         DBConn.close();
         return true;
     }
 
     public boolean update(Student student) {
+        Connection conn = DBConn.getConnection();
+        PreparedStatement psm = null;
+        String sql = "update student set birth = ? where id = ?";
+        try {
+            psm = conn.prepareStatement(sql);
+            psm.setDate(1, Date.valueOf(student.getBirth()));
+            psm.setLong(2, student.getId());
+            psm.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (psm != null) {
+                try {
+                    psm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DBConn.close();
         return true;
     }
 
     public boolean delete(long id) {
+        Connection conn = DBConn.getConnection();
+        PreparedStatement psm = null;
+        String sql = "delete from student where id = ?";
+        try {
+            psm = conn.prepareStatement(sql);
+            psm.setLong(1, id);
+            psm.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (psm != null) {
+                try {
+                    psm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DBConn.close();
         return true;
     }
 
     public Student get(long id) {
-        return null;
+        Connection conn = DBConn.getConnection();
+        PreparedStatement psm = null;
+        ResultSet resultSet = null;
+        Student student = null;
+        String sql = "select * from student where id = ?";
+        try {
+            psm = conn.prepareStatement(sql);
+            psm.setLong(1, id);
+            resultSet = psm.executeQuery();
+            if (resultSet.next()) {
+                student = new Student();
+                student.setId(resultSet.getLong("id"));
+                student.setName(resultSet.getString("name"));
+                student.setAge(resultSet.getInt("age"));
+                student.setBirth(resultSet.getDate("birth").toLocalDate());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psm != null) {
+                try {
+                    psm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DBConn.close();
+        return student;
     }
 
     public List<Student> getAll() {
-        return null;
+        Connection conn = DBConn.getConnection();
+        PreparedStatement psm = null;
+        ResultSet resultSet = null;
+        List<Student> students = new ArrayList<>();
+        String sql = "select * from student";
+        try {
+            psm = conn.prepareStatement(sql);
+            resultSet = psm.executeQuery();
+            while (resultSet.next()) {
+                Student student = new Student();
+                student.setId(resultSet.getLong("id"));
+                student.setName(resultSet.getString("name"));
+                student.setAge(resultSet.getInt("age"));
+                student.setBirth(resultSet.getDate("birth").toLocalDate());
+                students.add(student);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psm != null) {
+                try {
+                    psm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DBConn.close();
+        return students;
+    }
+
+    public int countAll() {
+        Connection conn = DBConn.getConnection();
+        int count = 0;
+        PreparedStatement psm = null;
+        ResultSet resultSet = null;
+        String sql = "select count(*) from student";
+        try {
+            psm = conn.prepareStatement(sql);
+            resultSet = psm.executeQuery();
+            if (resultSet.next()) {
+                count = resultSet.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (psm != null) {
+                try {
+                    psm.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        DBConn.close();
+        return count;
     }
 }
--- spring-jdbc-example/src/test/java/com/sjkim/jdbc2/StudentServiceTest.java
+++ spring-jdbc-example/src/test/java/com/sjkim/jdbc2/StudentServiceTest.java
@@ -1,11 +1,15 @@
 package com.sjkim.jdbc2;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class StudentServiceTest {
 
@@ -22,4 +26,64 @@ public class StudentServiceTest {
         assertThat(result, is(Boolean.TRUE));
     }
 
+    @Test
+    void updateStudentTest() throws Exception {
+        LocalDate birth = LocalDate.now().plusDays(1L);
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        Student student = students.get(0);
+        student.setBirth(birth);
+        boolean result = studentService.update(student);
+        assertEquals(Boolean.TRUE, result);
+
+        Student updatedStudent = studentService.get(student.getId());
+        assertThat(student.getId(), equalTo(updatedStudent.getId()));
+    }
+
+    @Test
+    void countAllTest() throws Exception {
+        int count = studentService.countAll();
+        List<Student> students = studentService.getAll();
+        assertEquals(students.size(), count);
+    }
+
+    @Test
+    void getAllTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        int count = studentService.countAll();
+        assertThat(students.size(), equalTo(count));
+    }
+
+    @Test
+    void deleteStudentTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        long id = students.get(0).getId();
+        boolean result = studentService.delete(id);
+        assertEquals(Boolean.TRUE, result);
+        Student deletedStudent = studentService.get(id);
+        assertNull(deletedStudent);
+        assertThat(deletedStudent, nullValue());
+    }
+
+    @Test
+    void getStudentTest() throws Exception {
+        List<Student> students = studentService.getAll();
+        if (students.isEmpty()) {
+            Student student = addStudent();
+            studentService.add(student);
+            students = studentService.getAll();
+        }
+        long id = students.get(0).getId();
+        Student getStudent = studentService.get(id);
+        Assertions.assertEquals(id, getStudent.getId());
+    }
 }
