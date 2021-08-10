c23c5f2a504d2a5eeaece114a31df1d2351278b3
sjkim <abbccc0225@gmail.com>
2021-08-10 23:04:28 +0900

    update spring-security.xml - url별 페이지 접근 테스트

--- spring-jpa-example/src/main/java/com/sjkim/controller/HomeController.java
+++ spring-jpa-example/src/main/java/com/sjkim/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.sjkim.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,5 +14,21 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/member")
+    public void doMember() {
+//        return "member";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/admin")
+    public String doAdmin() {
+        return "admin";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/error")
+    public String accessDenied(Authentication auth, Model model) {
+        model.addAttribute("auth", auth);
+        return "error";
     }
 }
