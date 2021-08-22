4130eb68f823b7afd8bc918ddd85df5f7e7e8e36
sjkim <abbccc0225@gmail.com>
2021-08-22 22:32:07 +0900

    add RoleRepository 추가

---
+++ spring-security-example/src/main/java/com/sjkim/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.sjkim.repository;
+
+import com.sjkim.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
