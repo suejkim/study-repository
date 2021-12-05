6d7532cf0b56ce3ce87ce10037410a2831ca404b
sjkim <abbccc0225@gmail.com>
2021-12-05 13:58:09 +0900

    update build.gradle

---
+++ spring-security-sample/server/basic-test/src/test/java/com/sjkim/RunTest.java
@@ -0,0 +1,13 @@
+package com.sjkim;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RunTest {
+
+    @Test
+    void runTest() {
+        assertThat("test").isEqualTo("test");
+    }
+}
