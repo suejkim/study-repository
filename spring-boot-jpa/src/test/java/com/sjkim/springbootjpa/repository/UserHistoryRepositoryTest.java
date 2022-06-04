c1796f551362281f48d48d0e95e074d91231f9bc
sjkim <abbccc0225@gmail.com>
2022-06-04 22:58:10 +0900

    UserHistory UserRepostory 추가

--- spring-boot-jpa/src/test/java/com/sjkim/springbootjpa/repository/UserHistoryRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/sjkim/springbootjpa/repository/UserHistoryRepositoryTest.java
@@ -1,2 +1,46 @@
-package com.sjkim.springbootjpa.repository;public class UserHistoryRepositoryTest {
+package com.sjkim.springbootjpa.repository;
+
+import com.sjkim.springbootjpa.domain.ActionType;
+import com.sjkim.springbootjpa.domain.Gender;
+import com.sjkim.springbootjpa.domain.User;
+import com.sjkim.springbootjpa.domain.UserHistory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class UserHistoryRepositoryTest {
+
+    @Autowired
+    private UserHistoryRepository userHistoryRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void addUserHistory() {
+        var user = buildUserRequest();
+        userRepository.save(user);
+        var userHistory = buildUserHistoryRequest(user);
+        userHistoryRepository.save(userHistory);
+    }
+
+    private User buildUserRequest() {
+        return User.builder()
+                .loginId("TEST2")
+                .password("test2")
+                .gender(Gender.MALE)
+                .mobile("01011113333")
+                .name("NAME")
+                .build();
+    }
+
+    private UserHistory buildUserHistoryRequest(User user) {
+        return UserHistory.builder()
+                .user(user)
+                .type(ActionType.BUY)
+                .build();
+    }
+
 }
--- spring-boot-jpa/src/test/java/com/sjkim/springbootjpa/repository/UserRepositoryTest.java
+++ spring-boot-jpa/src/test/java/com/sjkim/springbootjpa/repository/UserRepositoryTest.java
@@ -1,2 +1,36 @@
-package com.sjkim.springbootjpa.repository;public class UserRepositoryTest {
+package com.sjkim.springbootjpa.repository;
+
+
+import com.sjkim.springbootjpa.domain.Gender;
+import com.sjkim.springbootjpa.domain.User;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void addUser() {
+        var user = userRepository.save(buildUserRequest());
+        var users = userRepository.findAll();
+        log.info("{}", Lists.newArrayList(users).size());
+    }
+
+    private User buildUserRequest() {
+        return User.builder()
+                .loginId("TEST2")
+                .password("test2")
+                .gender(Gender.MALE)
+                .mobile("01011113333")
+                .name("NAME")
+                .build();
+    }
+
 }
